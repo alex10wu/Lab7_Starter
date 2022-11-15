df94d9149ad41afa183f348b0eec1df1bb94203d
Your Name <you@example.com>
2022-11-14 20:27:28 -0800

    Update explore

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -54,7 +54,30 @@ function initializeServiceWorker() {
   // B5. TODO - In the event that the service worker registration fails, console
   //            log that it has failed.
   // STEPS B6 ONWARDS WILL BE IN /sw.js
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', async () => {
+      // https://developer.mozilla.org/en-US/docs/Web/API/Navigator/serviceWorker
+      try {
+        const registration = await navigator.serviceWorker.register("/sw.js", {
+          scope: "/",
+        });
+        
+        if (registration.installing) {
+          console.log("Service worker installing");
+        } else if (registration.waiting) {
+          console.log("Service worker installed");
+        } else if (registration.active) {
+          console.log("Service worker active");
+        }
+      } catch (error) {
+        console.error(`Registration failed with ${error}`);
+      }
+    })
+  }
 }
+  
+
+
 
 /**
  * Reads 'recipes' from localStorage and returns an array of
